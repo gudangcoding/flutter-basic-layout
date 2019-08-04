7eb8eb38c70c4cef6bf5c6028ae9eab0f0e6252b
Your Name <dientrimedia@gmail.com>
2019-08-04 11:00:46 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HelloWorld(),
+    title: "Kursus Flutter",
+    routes: <String, WidgetBuilder>{
+      '/HelloWorld' : (BuildContext, context) => new HelloWorld(),
+    }
   ));
 }
 
@@ -10,12 +15,27 @@ class HelloWorld extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        body: new Container( //kotaknya
-        color: Colors.green[200],
-         child: new Center(
-           child: new Text("Kursus Flutter"),
-         ),
-        )
-    );
+        //urutan 1
+        appBar: new AppBar(
+          backgroundColor: Colors.green,
+          leading: new Icon(Icons.home),
+          title: new Text("Basic Layout"),
+          actions: <Widget>[new Icon(Icons.search)],
+        ),
+        //urutan 2
+        body: new Center(
+          child: new Container(  //kotaknya
+            color: Colors.green[200],
+            width: 200,
+            height: 200,
+
+            child: new Center(
+              child:new IconButton(
+                icon: new Icon(Icons.headset, size: 40,),
+                onPressed: null,
+              )
+            )
+          ),
+        ));
   }
 }
